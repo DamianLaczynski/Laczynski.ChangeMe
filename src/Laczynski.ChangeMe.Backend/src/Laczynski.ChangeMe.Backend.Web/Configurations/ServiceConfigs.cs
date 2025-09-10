d9e31be607415622b7e3db55efe5ca1f3f169928
Damian Łączyński <damian.laczynski.edu@gmail.com>
2025-09-10 21:11:56 +0200

    refactor: Update ServiceConfigs to Use WebApplicationBuilder Directly
    - Modified the AddServiceConfigs method to pass the WebApplicationBuilder instance directly to AddInfrastructureServices, improving clarity and consistency in service configuration.

--- src/Laczynski.ChangeMe.Backend/src/Laczynski.ChangeMe.Backend.Web/Configurations/ServiceConfigs.cs
+++ src/Laczynski.ChangeMe.Backend/src/Laczynski.ChangeMe.Backend.Web/Configurations/ServiceConfigs.cs
@@ -7,7 +7,7 @@ public static class ServiceConfigs
 {
   public static IServiceCollection AddServiceConfigs(this IServiceCollection services, Microsoft.Extensions.Logging.ILogger logger, WebApplicationBuilder builder)
   {
-    services.AddInfrastructureServices(builder.Configuration, logger)
+    services.AddInfrastructureServices(builder, logger)
             .AddMediatrConfigs()
             .AddCorsConfig(builder.Configuration);
 
